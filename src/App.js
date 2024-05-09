46f8b6c3281deaed9c60e9070edb0957e500dec7
Ganeshvedhachalam <ganeshgreat08@gmail.com>
2024-05-10 00:29:20 +0530

    stopwatch proj comp

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Mystopwatch from './MyStopWatch';
+
+function App() {
+  return (
+    <div className="App">
+      <Mystopwatch/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MyStopWatch.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from "react";
+
+function Mystopwatch(){
+
+    const [time, setTime ] =useState("0:00");
+    const [start,setStart] = useState(false );
+
+    const formattime = (seconds) => {
+        const mins = Math.floor(seconds/60).toString().padStart(2,"0")
+        const secs = (seconds%60).toString().padStart(2,"0")
+        return `${mins}:${secs}`
+    }
+
+
+    const handleStart = ()=>{
+        setStart((prev)=>!prev) // toggling true or false 
+
+    }
+     const handleReset = ()=>{
+        setTime("0:00")
+        setStart(false) //stops the timer running 
+    
+
+     }
+
+    useEffect(()=>{
+        let interval;
+    if (start) {
+        interval = setInterval(() => {
+            setTime(prevTime => {
+                const newTime = prevTime.split(":");
+                const mins = parseInt(newTime[0]);
+                const secs = parseInt(newTime[1]);
+
+                const totalSeconds = mins * 60 + secs + 1;
+                return formattime(totalSeconds);
+            });
+        }, 1000);
+            }else{
+                clearTimeout(interval)//clear the time 
+            }
+        
+
+        return ()=>
+            clearInterval(interval);
+        
+
+    },[start])
+
+    return(
+        
+        <div style={{display:"flex", justifyContent:"center" ,alignItems:"center" ,height:"400px" }} >
+            <div style={{display:"flex", flexDirection:"column", justifyContent:"center" ,alignItems:"center", 
+            height:"300px", width:"600px" ,border:"2px solid black"
+            }}>
+            <h1>Stopwatch</h1>
+            <h2>Time :{time}</h2>
+
+            <div>
+            <button onClick={handleStart} style={{height:"50px" ,width:"100px",margin:"10px"}}>
+                {
+                    start ? <h2> stop </h2> : <h2>start </h2>
+                }
+
+            </button>
+
+            <button  onClick={handleReset} style={{height:"50px" ,width:"100px",margin:"10px"}}><h2>reset </h2>
+
+            </button>
+            </div>
+           
+            </div>
+            
+           
+            
+
+
+        </div>
+        
+    )
+
+}
+export default Mystopwatch;
